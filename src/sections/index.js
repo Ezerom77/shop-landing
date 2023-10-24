633173bd0e585fb91db94663a8c4ebe7b862cfb1
Ezequiel Romera <eze@EzM1.local>
2023-10-24 08:24:20 -0300

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Hero from "./Hero";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import Suscribe from "./Suscribe";
+
+export {
+  CustomerReviews,
+  Hero,
+  Footer,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  SuperQuality,
+  Suscribe
+}
